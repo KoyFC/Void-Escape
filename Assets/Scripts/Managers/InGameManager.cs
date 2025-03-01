ebbd4290de971a19a7ea0a332da0239b6967023f
KoyFC <jorgekoy3@gmail.com>
2025-03-01 14:17:06 +0100

    v0.8.4b Arrow Rotation
    Rather than changing the arrow sprites, they now rotate to match the current horizontal or vertical state.
    
    The Main Menu ship color selection buttons have bit slightly adjusted to better differentiate the color the cursor is hovering over.

--- Assets/Scripts/Managers/InGameManager.cs
+++ Assets/Scripts/Managers/InGameManager.cs
@@ -18,7 +18,6 @@ public class InGameManager : MonoBehaviour
 
 
     [Header("Game Settings")]
-    public bool m_ChangePerspectiveNow = false;
     [HideInInspector] public bool m_IsHorizontal = true;
     public bool m_InvertedControls = false;
     public event Action OnPerspectiveChanged;
@@ -30,7 +29,6 @@ public class InGameManager : MonoBehaviour
     [Header("UI Settings")]
     [SerializeField] private Image m_LeftArrowImage = null;
     [SerializeField] private Image m_RightArrowImage = null;
-    [SerializeField] private Sprite[] m_ArrowSprites = null;
 
 
     private void Awake()
@@ -53,15 +51,6 @@ public class InGameManager : MonoBehaviour
     private void Start()
     {
         StartGame();
-    }
-
-    private void Update()
-    {
-        if (m_ChangePerspectiveNow)
-        {
-            m_ChangePerspectiveNow = false;
-            ChangePerspective();
-        }
     }
 
     private void SpawnSpaceship()
@@ -98,14 +87,13 @@ public class InGameManager : MonoBehaviour
 
     private IEnumerator ChangePerspective()
     {
+        bool returnToOriginal = false; // Used to determine if the arrows should rotate back to their original position
         yield return new WaitForSeconds(1);
 
         while (true)
         {
             yield return new WaitForSeconds(4);
 
-            // Wait until the player stops moving so we can ensure no variables
-            // are modified by the movement script during the transition
             yield return new WaitUntil(() => !m_PlayerController.m_PlayerMovement.m_IsMoving);
 
             m_CameraStateAnimator.SetTrigger("ChangePerspective");
@@ -113,16 +101,38 @@ public class InGameManager : MonoBehaviour
 
             m_PlayerController.m_PlayerMovement.m_IsMoving = true;
 
+            StartCoroutine(RotateSprite(m_LeftArrowImage, -90f, returnToOriginal));
+            StartCoroutine(RotateSprite(m_RightArrowImage, -90f, returnToOriginal));
+
             yield return new WaitForSeconds(0.75f);
 
-            OnPerspectiveChanged?.Invoke(); // Notify the player movement script that the perspective has changed
-
-            m_LeftArrowImage.sprite = m_IsHorizontal ? m_ArrowSprites[2] : m_ArrowSprites[0];
-            m_RightArrowImage.sprite = m_IsHorizontal ? m_ArrowSprites[3] : m_ArrowSprites[1];
+            OnPerspectiveChanged?.Invoke();
 
             yield return new WaitForSeconds(0.25f);
 
             m_PlayerController.m_PlayerMovement.m_IsMoving = false;
+
+            returnToOriginal = !returnToOriginal;
         }
+    }
+
+    private IEnumerator RotateSprite(Image image, float newZRotation, bool returnToOriginal)
+    {
+        Quaternion startRotation = image.transform.rotation;
+
+        Quaternion endRotation = returnToOriginal ? 
+            Quaternion.Euler(0, 0, 0) :
+            Quaternion.Euler(0, 0, newZRotation);
+
+        float elapsedTime = 0.0f;
+
+        while (elapsedTime < 0.5f)
+        {
+            image.transform.rotation = Quaternion.Slerp(startRotation, endRotation, elapsedTime / 0.5f);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        image.transform.rotation = endRotation;
     }
 }
